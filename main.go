13c9b608a1f190ae1d920723cafdd7ac84f76400
deadmau5v <3230772301@qq.com>
2024-04-18 20:31:53 +0800

    add

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"LoongPanel/Panel/Status"
-	"fmt"
-	"time"
-)
+import "LoongPanel/Panel/Files"
 
 func main() {
-	for {
-		time.Sleep(1 * time.Second)
-		fmt.Println(
-			"负荷: ", Status.LoadAverage1m(),
-			fmt.Sprintf(" CPU: %.2f", Status.CPUPercent()),
-			fmt.Sprintf(" Memroy: %.2f", Status.MemroyPercent()),
-		)
-
-	}
-
+	Files.Dir("/")
 }
