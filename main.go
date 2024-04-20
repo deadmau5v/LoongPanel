be1072fc4e5a3d15586e93f362a7cc20b161c973
deadmau5v <3230772301@qq.com>
2024-04-20 22:36:11 +0800

    feat: 增加前端

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"LoongPanel/Panel/Files"
-	"fmt"
-)
-
 func main() {
-	dir, _ := Files.Dir("D:\\code\\golang\\LoongPanel\\test")
-	//	dir, _ := Files.Dir("/root/LoongPanel/test")
-	for _, file := range dir {
-		fmt.Println(
-			"文件:", file.Name,
-		)
-		toFile := file.CopyObj()
 
-		f := toFile.SetPath("D:\\code\\golang\\LoongPanel\\test\\test2")
-		err := file.Move(f)
-		if err != nil {
-			fmt.Println("错误", err.Error())
-			return
-		}
-
-	}
 }
