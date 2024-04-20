f2127affed19e3d5b8f9a8c085a5df6a31c23530
deadmau5v <3230772301@qq.com>
2024-04-20 20:15:24 +0800

    feat: 增加前端

--- Panel/Files/Method.go
+++ Panel/Files/Method.go
@@ -3,9 +3,11 @@ package Files
 import (
 	"LoongPanel/Panel/System"
 	"errors"
+	"fmt"
 	"github.com/spf13/afero"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -44,8 +46,8 @@ func (f *File) Create() error {
 	return nil
 }
 
-// setName 设置文件名 同步修改文件路径
-func (f *File) setName(name string) error {
+// SetName 设置文件名 同步修改文件路径
+func (f *File) SetName(name string) error {
 	ok := CheckFileName(name)
 	if !ok {
 		return errors.New("文件名不合法")
@@ -60,8 +62,28 @@ func (f *File) setName(name string) error {
 	return nil
 }
 
-// NewFile 返回默认对象
-func NewFile() *File {
+// SetPath 设置文件路径
+func (f *File) SetPath(path string) *File {
+	f_ := f.CopyObj()
+	if path[len(path)-1] == os.PathSeparator {
+		f_.Path = path + f_.Name
+	} else {
+		f_.Path = path + string(os.PathSeparator) + f_.Name
+	}
+	return f_
+}
+
+// GetDir 获取文件的纯目录 (不包含文件名)
+func (f *File) GetDir() string {
+	name := []rune(f.Name)
+	dir := []rune(f.Path)
+	dir = dir[:len(dir)-len(name)]
+	fmt.Println(string(dir))
+	return string(dir)
+}
+
+// NewObj 返回默认对象
+func NewObj() *File {
 	return &File{
 		Name:  "NewFile",
 		Size:  0,
@@ -129,4 +151,37 @@ func (f *File) Copy(dst *File) error {
 
 	}
 	return nil
+}
+
+// Rename 重命名文件
+func (f *File) Rename(dst *File) error {
+	if f.Path == dst.Path {
+		return errors.New("源文件与目标文件路径相同")
+	} else if strings.Contains(dst.Path, f.Path) {
+		return errors.New(fmt.Sprintf("目标文件路径包含源文件路径 %s -> %s", f.Path, dst.Path))
+	}
+	fs := afero.NewOsFs()
+	err := fs.Rename(f.Path, dst.Path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CopyObj 复制对象
+func (f *File) CopyObj() *File {
+	return &File{
+		Name:  f.Name,
+		Size:  f.Size,
+		Time:  f.Time,
+		IsDir: f.IsDir,
+		Mode:  f.Mode,
+		Path:  f.Path,
+		Ext:   f.Ext,
+	}
+}
+
+// Move 移动文件
+func (f *File) Move(dst *File) error {
+	return f.Rename(dst)
 }
--- main.go
+++ main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func main() {
-	dir, _ := Files.Dir("C:/")
+	dir, _ := Files.Dir("D:\\code\\golang\\LoongPanel\\test")
+	//	dir, _ := Files.Dir("/root/LoongPanel/test")
 	for _, file := range dir {
 		fmt.Println(
-			"Name:", file.Name,
-			"Ext:", file.Ext,
+			"文件:", file.Name,
 		)
+		toFile := file.CopyObj()
+
+		f := toFile.SetPath("D:\\code\\golang\\LoongPanel\\test\\test2")
+		err := file.Move(f)
+		if err != nil {
+			fmt.Println("错误", err.Error())
+			return
+		}
 
 	}
 }
