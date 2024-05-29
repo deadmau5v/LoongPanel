5db3d299721a21e033452d26181378644c6a5ab3
deadmau5v <3230772301@qq.com>
2024-05-30 07:12:51 +0800

    feat: 增加面板日志支持

--- Panel/Service/LogManage/PanelLog/Func.go
+++ Panel/Service/LogManage/PanelLog/Func.go
@@ -7,14 +7,64 @@
 package PanelLog
 
 import (
+	Log2 "LoongPanel/Panel/Service/Log"
 	"LoongPanel/Panel/Service/LogManage"
+	"io"
+	"os"
+	"strings"
 )
 
 func GetPanelLog() *LogManage.Log_ {
 
-	return &LogManage.Log_{
+	log := &LogManage.Log_{
 		Path: "panel.log",
 		Name: "面板日志",
 		Ok:   true,
 	}
+
+	log.Ok = log.CheckLog_Exist()
+	if !log.Ok {
+		return nil
+	}
+
+	log.GetLog = func(line int) []byte {
+		file, err := os.Open(log.Path)
+		if err != nil {
+			Log2.ERROR("[面板日志] 打开日志文件失败: ", err.Error())
+			return nil
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				Log2.ERROR("[面板日志] 关闭日志文件失败: ", err.Error())
+			}
+		}(file)
+
+		all, err := io.ReadAll(file)
+		if err != nil {
+			Log2.ERROR("[面板日志] 读取日志文件失败: ", err.Error())
+			return nil
+		}
+
+		allStr := string(all)
+		allStrSplite := strings.Split(allStr, "\n")
+		if len(allStrSplite) > line {
+			allStrSplite = allStrSplite[len(allStrSplite)-line:]
+			all = []byte(strings.Join(allStrSplite, "\n"))
+		}
+
+		return all
+	}
+
+	log.ClearLog = func() {
+		if !log.Ok {
+			return
+		}
+		err := os.Truncate(log.Path, 0)
+		if err != nil {
+			Log2.ERROR("[面板日志] 清空日志文件失败: ", err.Error())
+		}
+	}
+
+	return log
 }
