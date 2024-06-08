a592a7ceef0eb86a9626bd1696f4a0b11f30114d
deadmau5v <3230772301@qq.com>
2024-06-08 13:01:18 +0800

    feat: 添加自动安装脚本

---
+++ Script/install_tidb.sh
@@ -0,0 +1,30 @@
+#
+# 创建人： deadmau5v
+# 创建时间： 2024-6-8
+# 文件作用：安装frp
+#
+
+# 检查权限
+if [ $(id -u) -ne 0 ]; then
+    echo "请使用root权限运行"
+    exit 1
+fi
+
+# 下载
+wget https://cdn1.d5v.cc/CDN/File/tidb-server
+
+# 安装
+mkdir /opt/tidb -p
+mv tidb-server /opt/tidb/tidb-server
+chmod +x /opt/tidb/tidb-server
+
+# 环境变量
+echo "export PATH=\$PATH:/opt/tidb" >> ~/.bashrc
+source ~/.bashrc
+
+# 测试
+if [ -x "$(command -v tidb-server)" ]; then
+    echo "安装成功"
+else
+    echo "安装失败"
+fi
