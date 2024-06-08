cb0ce46067c8f720b3b4d8c9fd19020bdd707d99
deadmau5v <3230772301@qq.com>
2024-06-08 23:33:02 +0800

    fix: 调试terminal

--- Panel/API/v1/terminal/websocket.go
+++ Panel/API/v1/terminal/websocket.go
@@ -1,3 +1,9 @@
+/*
+ * 创建人： deadmau5v
+ * 创建时间： 2024-5-7
+ * 文件作用：提供 terminal 的流式传输
+ */
+
 package terminal
 
 import (
@@ -20,72 +26,82 @@ func ScreenWs(c *gin.Context) {
 	PanelLog.INFO("[网页终端]", "ScreenWs WebSocket 连接")
 	w := c.Writer
 	r := c.Request
-	PanelLog.DEBUG("[网页终端]", "升级连接")
 	conn, err := upgrade.Upgrade(w, r, nil)
 	if err != nil {
-		PanelLog.ERROR("[网页终端] 无法打开 WebSocket 连接: ", err)
+		PanelLog.ERROR("[网页终端] 无法打开websocket连接")
 		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 		return
 	}
 	defer func(conn *websocket.Conn) {
-		PanelLog.DEBUG("[网页终端]", "WebSocket 连接关闭")
+		PanelLog.DEBUG("[网页终端]", "websocket 连接关闭")
 		err := conn.Close()
 		if err != nil {
-			PanelLog.ERROR("[网页终端]", "Ws 链接异常关闭: ", err)
+			PanelLog.ERROR("[网页终端]", "Ws链接异常关闭")
 		}
 	}(conn)
 
-	PanelLog.DEBUG("[网页终端]", "获取参数")
 	id := getIntQuery(c, "id")
 	screen := Terminal.MainScreenManager.GetScreen(uint32(id))
 	if screen.WS != nil {
 		err := screen.WS.Close()
 		if err != nil {
-			PanelLog.ERROR("[网页终端] 关闭原有连接失败: ", err)
+			PanelLog.DEBUG("[网页终端] 关闭原有连接失败")
 			return
 		}
+		screen.WS = conn
 	}
-	screen.WS = conn
-	PanelLog.DEBUG("[网页终端]", "创建新连接")
 	input := make(chan []byte, 1024)
 	output := screen.Subscribe()
 	defer close(input)
 
-	go func() {
-		PanelLog.DEBUG("[网页终端]", "开始发送数据")
-		for {
-			select {
-			case <-time.After(100 * time.Millisecond):
-				screen.Publish()
+	close_ := func() {
+		PanelLog.DEBUG("[网页终端] websocket 关闭中...")
+		id_, name_ := screen.Id, screen.Name
+		if screen.WS != nil && screen.WS == conn {
+			err := conn.Close()
+			if err != nil {
+				PanelLog.DEBUG("[网页终端] 关闭连接失败")
+				return
 			}
+			screen.Close()
+			conn = nil
+			screen.WS = nil
+		}
+
+		Terminal.MainScreenManager.Close(id)
+		// 重建screen
+
+		_ = Terminal.MainScreenManager.Create(name_, id_)
+
+		c.Abort()
+	}
+
+	go func() {
+		for {
+			screen.Publish()
+			time.Sleep(100)
 		}
 	}()
 
 	go func() {
-		PanelLog.DEBUG("[网页终端]", "开始读取数据")
 		for {
 			if conn != nil {
 				_, message, err := conn.ReadMessage()
 				if err != nil {
-					PanelLog.ERROR("[网页终端] 读取消息失败: ", err)
-					// 关闭连接
-					conn.Close()
+					close_()
 					return
 				}
 				input <- message
 			}
 		}
 	}()
-
 	for {
 		if conn != nil {
 			select {
 			case o := <-output:
-				err := conn.WriteMessage(websocket.TextMessage, o)
+				err := conn.WriteMessage(1, o)
 				if err != nil {
-					PanelLog.ERROR("[网页终端] 发送数据失败: ", err)
-					// 关闭连接
-					conn.Close()
+					close_()
 					return
 				}
 			case i := <-input:
